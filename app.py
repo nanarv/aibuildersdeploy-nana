ff75a1aaae58cb049d43f87235e9b2f8a1eeed45
Rasita Vajapattana <122709179+nanarv@users.noreply.github.com>
2023-06-13 21:54:56 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, jsonify
+from fastai.basic_train import load_learner
+from fastai.vision import open_image
+from flask_cors import CORS,cross_origin
+app = Flask(__name__)
+CORS(app, support_credentials=True)
+# code:
+import locale
+import pandas as pd
+import os
+import torch
+from torch import Tensor
+from fastai import *
+from fastai.vision import *
+from torchvision import transforms
+import collections
+import collections.abc
+import matplotlib.pyplot as plt
+from PIL import Image
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import cohen_kappa_score
+
+def quadratic_kappa(y_hat, y):
+    return torch.tensor(cohen_kappa_score(torch.argmax(y_hat.cpu(),1), y.cpu(), weights='quadratic'),device='cuda:0')
+
+# load the learner
+learn = load_learner(r'model')
+classes = learn.data.classes
+
+
+def predict_single(img_file):
+    'function to take image and return prediction'
+    prediction = learn.predict(open_image(img_file))
+    probs_list = prediction[2].numpy()
+    return {
+        'category': classes[prediction[1].item()],
+        'probs': {c: round(float(probs_list[i]), 5) for (i, c) in enumerate(classes)}
+    }
+
+
+# route for prediction
+@app.route('/predict', methods=['POST'])
+def predict():
+    return jsonify(predict_single(request.files['image']))
+
+if __name__ == '__main__':
+    app.run()
